df6986ea5bb31f1dc9f6e110b34fe42f30696df3
cruse1914 <contact.dm07@gmail.com>
2021-03-17 01:04:37 +0530

    Updates

---
+++ precache-manifest.f5820a3df31081960730aadcbffb5bcd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25b30a10569f56c81e2d151f21069cc",
+    "url": "/Subscribe/index.html"
+  },
+  {
+    "revision": "5555a263e91bc803a0da",
+    "url": "/Subscribe/static/js/2.84f7f433.chunk.js"
+  },
+  {
+    "revision": "c848b38816bce3a4e2ae",
+    "url": "/Subscribe/static/js/main.89fc9280.chunk.js"
+  },
+  {
+    "revision": "73684dde2df908c52699",
+    "url": "/Subscribe/static/js/runtime-main.43dfb0a5.js"
+  }
+]);
